0b6b3be496d89a38736f32da92f1b610d0bba378
SR <srdev@naver.com>
2021-01-22 22:34:22 +0900

    3D mesh

--- Assets/SpriteAssist/Editor/Util/SpriteExtension.cs
+++ Assets/SpriteAssist/Editor/Util/SpriteExtension.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 namespace SpriteAssist
 {
+    public struct Edge
+    {
+        public int t1;
+        public int t2;
+ 
+        public Edge(int t1, int t2)
+        {
+            this.t1 = t1;
+            this.t2 = t2;
+        }
+
+        public Edge GetSwapped()
+        {
+            return new Edge(t2, t1);
+        }
+    }
+ 
     public static class SpriteExtension
     {
         public static void SetSpriteScaleToVertices(this Sprite sprite, Vector2[] vertices, float additionalScale, bool isFlipY, bool clamp)
@@ -32,15 +51,14 @@ namespace SpriteAssist
             }
         }
 
-        public static void GetMeshData(this Sprite sprite, SpriteConfigData data, out Vector2[] vertices, out ushort[] triangles, MeshRenderType meshRenderType)
+        public static void GetVertexAndTriangle(this Sprite sprite, SpriteConfigData data, out Vector2[] vertices, out ushort[] triangles, MeshRenderType meshRenderType)
         {
-            if (!TryGetMeshData(sprite, data, out vertices, out triangles, meshRenderType))
+            if (!TryVertexAndTriangle(sprite, data, out vertices, out triangles, meshRenderType))
             {
                 //fallback
                 vertices = sprite.vertices;
                 triangles = sprite.triangles;
             }
-
         }
 
         public static string GetMeshAreaInfo(this Sprite sprite, Vector2[] vertices, ushort[] triangles)
@@ -62,7 +80,7 @@ namespace SpriteAssist
             return $"{vertices.Length} verts, {triangles.Length / 3} tris, {meshAreaRatio:F2}% overdraw";
         }
 
-        private static bool TryGetMeshData(Sprite sprite, SpriteConfigData data, out Vector2[] vertices, out ushort[] triangles, MeshRenderType meshRenderType)
+        private static bool TryVertexAndTriangle(Sprite sprite, SpriteConfigData data, out Vector2[] vertices, out ushort[] triangles, MeshRenderType meshRenderType)
         {
             vertices = Array.Empty<Vector2>();
             triangles = Array.Empty<ushort>();
@@ -73,8 +91,9 @@ namespace SpriteAssist
             }
 
             Vector2[][] paths = OutlineUtil.GenerateOutline(sprite, data, meshRenderType);
-            TriangulationUtil.Triangulate(paths, data.edgeSmoothing, data.useNonZero, out vertices, out triangles);
 
+            TriangulationUtil.Triangulate(paths, data.edgeSmoothing, data.useNonZero, out vertices, out triangles);
+            
             //validate
             if (vertices.Length >= ushort.MaxValue)
             {
@@ -85,18 +104,56 @@ namespace SpriteAssist
             return true;
         }
 
-        public static void UpdateMesh(this Sprite sprite, ref Mesh mesh, Vector2[] v, ushort[] t)
+        public static void UpdateMesh(this Sprite sprite, ref Mesh mesh, Vector2[] v, ushort[] tUShort)
         {
-            Vector2[] uv = new Vector2[v.Length];
+            int[] t = Array.ConvertAll(tUShort, i => (int)i);
+
+            Vector3[] v2 = new Vector3[v.Length * 2];
+            int[] t2 = new int[t.Length * 2];
+
+            for (var i = 0; i < v.Length; i++)
+            {
+                v2[i + v.Length * 0] = new Vector3(v[i].x, v[i].y, -0.5f);
+                v2[i + v.Length * 1] = new Vector3(v[i].x, v[i].y, 0.5f);
+            }
+
+            for (var i = 0; i < t.Length; i++)
+            {
+                //front
+                t2[i + 0] = t[i + 0];
+
+                //back(reversed)
+                t2[t.Length * 2 - 1 - i] = t[i] + v.Length;
+            }
+
+            var edges = GetMeshEdges(t);
+            List<int> t3 = new List<int>();
+
+            foreach (var edge in edges)
+            {
+                t3.Add(edge.t2);
+                t3.Add(edge.t1);
+                t3.Add(edge.t1 + v.Length);
+
+                t3.Add(edge.t1 + v.Length);
+                t3.Add(edge.t2 + v.Length);
+                t3.Add(edge.t2);
+            }
+
+            var temp = t2.ToList();
+            temp.AddRange(t3);
+            t2 = temp.ToArray();
+
+            Vector2[] uv = new Vector2[v2.Length];
 
             for (var i = 0; i < uv.Length; i++)
             {
-                uv[i] = v[i] * sprite.pixelsPerUnit + sprite.pivot;
+                uv[i] = new Vector2(v2[i].x, v2[i].y) * sprite.pixelsPerUnit + sprite.pivot;
                 uv[i].x /= sprite.texture.width;
                 uv[i].y /= sprite.texture.height;
             }
 
-            Vector3[] normals = new Vector3[v.Length];
+            Vector3[] normals = new Vector3[v2.Length];
 
             for (var i = 0; i < normals.Length; i++)
             {
@@ -104,10 +161,44 @@ namespace SpriteAssist
             }
 
             mesh.Clear();
-            mesh.SetVertices(Array.ConvertAll(v, i => (Vector3)i));
+            mesh.SetVertices(v2);
             mesh.SetUVs(0, uv);
             mesh.SetNormals(normals);
-            mesh.SetTriangles(Array.ConvertAll(t, i => (int)i), 0);
+            mesh.SetTriangles(t2, 0);
+        }
+        
+        private static Edge[] GetMeshEdges(int[] triangles)
+        {
+            HashSet<Edge> edges = new HashSet<Edge>();
+            
+            for (int i = 0; i < triangles.Length; i += 3)
+            {
+                int t1 = triangles[i];
+                int t2 = triangles[i + 1];
+                int t3 = triangles[i + 2];
+
+                AddOrRemoveEdge(edges, new Edge(t1, t2));
+                AddOrRemoveEdge(edges, new Edge(t2, t3));
+                AddOrRemoveEdge(edges, new Edge(t3, t1));
+            }
+
+            return edges.ToArray();
+        }
+
+        private static void AddOrRemoveEdge(HashSet<Edge> edges, Edge edge)
+        {
+            if (edges.Contains(edge))
+            {
+                edges.Remove(edge);
+            }
+            else if (edges.Contains(edge.GetSwapped()))
+            {
+                edges.Remove(edge.GetSwapped());
+            }
+            else
+            {
+                edges.Add(edge);
+            }
         }
 
         public static GameObject CreateEmptyMeshPrefab(this Sprite sprite, bool hasSubObject)
