3dcae529846f95833f0bf5efc88413bb048d27a1
park.saerom <park.jungkyu@boi.jp>
2021-04-14 11:16:09 +0900

    Fix null shader issue

---
+++ Assets/SpriteAssist/Editor/Util/ShaderUtil.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace SpriteAssist
+{
+    public static class ShaderUtil
+    {
+        public const string RENDER_SHADER_TRANSPARENT = "Unlit/Transparent";
+        public const string RENDER_SHADER_OPAQUE = "Unlit/Texture";
+
+        public static Shader FindShader(string name, string fallback)
+        {
+            Shader shader = null;
+
+            if (string.IsNullOrEmpty(name) == false)
+            {
+                shader = Shader.Find(name);
+            }
+
+            if (shader == null)
+            {
+                shader = Shader.Find(fallback);
+            }
+
+            return shader;
+        }
+
+        public static Shader FindOpaqueShader(string name)
+        {
+            return FindShader(name, RENDER_SHADER_OPAQUE);
+        }
+
+        public static Shader FindTransparentShader(string name)
+        {
+            return FindShader(name, RENDER_SHADER_TRANSPARENT);
+        }
+    }
+}
